da0ae46c90d7c611063fb895d3d3d9f0eecc52f1
ainmat <mateuz.carvalho.s@gmail.com@exemplo.com>
2026-03-26 23:24:08 -0300

    Fix: alterando scripts para type=module e corrigindo 404 no build do Vite

--- dist/assets/detalhe-legacy-DZaInSqw.js
+++
@@ -1,2 +0,0 @@
-System.register(["./styles-legacy-9OJ7Hgy2.js"],function(e,t){"use strict";return{setters:[null],execute:function(){}}});
-//# sourceMappingURL=detalhe-legacy-DZaInSqw.js.map
--- dist/assets/todos-B_58ipeQ.js
+++
@@ -1,2 +0,0 @@
-function t(){import.meta.url,import("_").catch(()=>1),async function*(){}().next()}/* empty css               */export{t as __vite_legacy_guard};
-//# sourceMappingURL=todos-B_58ipeQ.js.map
